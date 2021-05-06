9cbffb9e46582d83dca362fbd4ded1ff79565f5a
Alex Daltsis <alexdaltsis@gmail.com>
2021-05-07 01:31:46 +0300

    Adds log

--- src/handlers/remove-urls.ts
+++ src/handlers/remove-urls.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { ShortUrl } from '../models/short-url';
 export async function removeUrls(req: express.Request, res: express.Response): Promise<any> {
     const deleteKey = req.body.deleteKey;
+    console.log({deleteKey});
+    
     if (deleteKey !== process.env.DELETE_KEY) {
         return res.status(403).json({ success: false, message: 'NOT AUTHORIZED' });
     }
