7dd66be08794400caf69538b1e5b2df914356c2c
Alex Daltsis <alexdaltsis@gmail.com>
2022-03-09 20:43:12 +0200

    Fix clipboard issue for http

--- front-end/index.js
+++ front-end/index.js
@@ -41,12 +41,35 @@ async function setUrl() {
 
 copyButton.addEventListener('click', async (ev) => {
     const url = urlTextField.value;
-    await navigator.clipboard.writeText(url);
+    await copyToClipboard(url);
     console.log({ url });
     // show snack bar
     showSnack();
 });
 
+function copyToClipboard(textToCopy) {
+    // navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        // navigator clipboard api method'
+        return navigator.clipboard.writeText(textToCopy);
+    } else {
+        // text area method
+        let textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        // make the textarea out of viewport
+        textArea.style.position = "fixed";
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        return new Promise((res, rej) => {
+            // here the magic happens
+            document.execCommand('copy') ? res() : rej();
+            textArea.remove();
+        });
+    }
+}
 
 
 function showSnack() {
